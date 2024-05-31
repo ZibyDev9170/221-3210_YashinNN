ace895d5a358a9ed4ec953155d68769515fd9ac8
ZibyDev9071 <nik.yashin.03@inbox.ru>
2024-05-31 14:08:47 +0300

    Finish lab2 in code but not in debug

---
+++ lab2/SimpleReadWrite/SimpleReadWrite/SimpleReadWrite.cpp
@@ -0,0 +1,98 @@
+﻿/*
+*	YASHIN
+*/
+
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+/*
+*	ДЛЯ ЧТЕНИЯ ФАЙЛА ПРИМЕНЯЕТСЯ ФЛАГ -r
+*	ДЛЯ ЗАПИСИ ФАЙЛА ПРИМЕНЯЕТСЯ ФЛАГ -w
+*/
+
+bool readFile(char path[]) {
+	std::cout << "READING FROM: " << path << "\n\n";
+
+	std::ifstream fin;
+
+	fin.open(path);
+
+	if (!fin) {
+		std::cout << "ERROR -> CANNOT READING FROM: " << path << "\n";
+		fin.close();
+		return false;
+	}
+	else {
+		std::string line;
+		while (std::getline(fin, line)) {
+			std::cout << line << "\n";
+		}
+	}
+
+	fin.close();
+
+	return true;
+}
+
+bool writeToFile(char path[], const char text[]= "Krestiki Noliki\nDve Milani v domike\nDve Milani\nUkatilis nashi shariki za roliki") {
+	std::cout << "WRITTING TO: " << path << "\n";
+
+	std::ofstream fout;
+
+	fout.open(path);
+
+	if (!fout) {
+		std::cout << "ERROR -> CANNOT WRITTEN TO: " << path << "\n";
+		fout.close();
+		return false;
+	}
+	else {
+		fout << text;
+		fout.close();
+	}
+
+
+	return true;
+}
+
+int main(int argc, char* argv[]) 
+{
+	if (argc == 3) 
+	{
+		if (!strcmp(argv[1], "-r")) 
+		{ 
+			readFile(argv[2]);
+		}
+		else if (!strcmp(argv[1], "-w")) 
+		{
+			writeToFile(argv[2]);
+		}
+		else
+		{
+			std::cout << "ERROR\n";
+		}
+	}
+
+	else if (argc == 4)
+	{
+		if (!strcmp(argv[1], "-w"))
+		{
+			writeToFile(argv[2], argv[3]);
+		}
+
+		else
+		{
+			std::cout << "ERROR\n";
+		}
+	}
+
+	else 
+	{
+		std::cout << "ERROR\n";
+	}
+
+
+	return 0;
+}
